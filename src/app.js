ffca004174a56533f99e4b8255741593b2af9972
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-01-18 01:24:45 +0000

    priemors cambios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = [":diamantes:", ":corazones:", ":picas:", ":tréboles:"];
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let elem = document.querySelectorAll(".palo");
+  let palo = palos[Math.floor(Math.random() * (4 - 0) + 0)];
+  for (let index = 0; index < elem.length; index++) {
+    elem[index].innerHTML = palo;
+  }
+  let numero = numeros[Math.floor(Math.random() * (13 - 0) + 0)];
+  document.querySelector(".numero").innerHTML = numero;
 };
