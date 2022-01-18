3373872d76c26d086d9bbe07d5120ca3a3c2d51e
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-01-18 01:27:50 +0000

    cambio palos y numeros

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let palos = [":diamantes:", ":corazones:", ":picas:", ":tréboles:"];
+  let palos = ["♥", "♥", "♠", "♣"];
   let numeros = [
     "A",
     "2",
